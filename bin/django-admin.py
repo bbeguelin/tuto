49fd4073b33d6ded10a44c9adcddff84ea127ece
bbeadmin <bbeguelin@altea-services.ch>
2014-10-01 12:04:10 +0200

    Initial Commit of Tuto BBE

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bbeadmin/.virtualenvs/django_tuto/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.7','django-admin.py'
+__requires__ = 'Django==1.7'
+import pkg_resources
+pkg_resources.run_script('Django==1.7', 'django-admin.py')
